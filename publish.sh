68f2a6e62c9fa735a6724a2f1c4e06268a7ed0d9
guoruizhong <grgrzhong@gmail.com>
2026-08-22 10:38:46 +0800

    Update notes

--- publish.sh
+++ publish.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-### completely remove git
-# rm -rf .git
 
+# rm -rf .git
 # git init
 # git remote remove origin
 # git checkout -b main
 # git branch -M main
 # git remote add origin https://github.com/grgrzhong/grgrzhong.github.io.git
 
+# Create the envrionment
+# conda create -n renv \
+#     r=4.5 \
+#     python \
+#     jupyter \
+#     r-languageserver \
+#     r-tidyverse \
+#     r-irkernel \
+#     r-httpgd \
+#     r-downlit \
+#     r-xml2
+
 # source $(conda info --base)/etc/profile.d/conda.sh
+
 conda activate renv
 
-### render site files to docs
 quarto render
 
-### add to the local git repository
 git add .
-# git add docs
-
-git commit -m "Update blogs"
-
-### push to the remote repository
+git commit -m "Update notes"
 git push -u origin main --force
